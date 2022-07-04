183ebeb230e7749126a6e510d219ad462e3cae17
Lucas <lucasvalenteds@gmail.com>
2022-07-04 00:10:27 -0300

    Implementing Person entity
    Creating the database migration, entity and repository.

---
+++ src/main/java/com/example/person/Person.java
@@ -0,0 +1,33 @@
+package com.example.person;
+
+import com.example.document.Document;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "PERSON")
+@Data
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PERSON_ID_SEQUENCE_GENERATOR")
+    @SequenceGenerator(name = "PERSON_ID_SEQUENCE_GENERATOR", sequenceName = "PERSON_ID_SEQUENCE", allocationSize = 1)
+    @Column(name = "PERSON_ID")
+    private Long id;
+
+    @Column(name = "PERSON_NAME")
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "DOCUMENT_ID")
+    private Document document;
+}
